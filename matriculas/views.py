d17c9eafa24490b3dae7ea38df3d06ab5a172b64
kennedywc <kennedy.costan10@gmail.com>
2023-10-26 16:21:32 -0300

    atv

--- matriculas/views.py
+++ matriculas/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Aluno
+from .forms import AlunoForm
 
 # Create your views here.
 def listar_alunos(request):
+    if request.method == 'POST':
+        form = AlunoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('alunos:listar_alunos')
+    else:
+        form = AlunoForm()
+
     alunos = Aluno.objects.all()
-    return render(request, 'listar.html', {'alunos': alunos})
+    return render(request, 'listar.html', {'alunos': alunos, 'form': form})
